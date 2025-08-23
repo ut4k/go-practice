e48ee0a623925beeb5e1cfe9ee563b64b4f1aeb1
ut4k <aquarium011@gmail.com>
2025-08-23 15:49:12 +0900

    リカバリーミドルウェア

---
+++ middleware/recover/main.go
@@ -0,0 +1,24 @@
+package recover
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func RecoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				jsonBody, _ := json.Marshal(map[string]string{
+					"error": fmt.Sprintf("%v", err),
+				})
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write(jsonBody)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
